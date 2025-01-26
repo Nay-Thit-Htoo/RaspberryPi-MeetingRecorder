c9b0e849fe0539358e87e05837cc00973ac951eb
Nay Thit Htoo <naythithtoo@abank.com.mm>
2025-01-26 18:13:11 +0630

    fix: close sox when stop btn click

--- audio_recorder.py
+++ audio_recorder.py
@@ -79,6 +79,8 @@ class AudioRecorder:
         self.recording = False
         if self.record_thread is not None:
             self.record_thread.join()
+        self.sox_process.stdin.close()
+        self.sox_process.wait() 
     
     def save_wave(self):
         self.create_folder_record_user()
