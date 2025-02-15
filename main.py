0413bd38cf04811f65c8a176235a8b7cbc4be2e1
Nay Thit Htoo <naythithtoo@abank.com.mm>
2025-02-15 17:43:38 +0630

    fix: main window close

--- main.py
+++ main.py
@@ -71,14 +71,13 @@ class Main(tk.Tk):
         self.protocol("WM_DELETE_WINDOW", self.on_close)
 
     def on_close(self):
+        self.destroy()
         if(self.frame.__class__.__name__=="MeetingRecordChairman"):
             if hasattr(self.frame, "close_window_chairman"):           
-             self.frame.close_window_chairman()             
-             self.destroy()
+             self.frame.close_window_chairman()
         elif(self.frame.__class__.__name__=="MeetingRecord"):
             if hasattr(self.frame, "close_window_client"):           
              self.frame.close_window_client()
-             self.destroy()
 
     def resize_background(self, event=None):
         # Get the current window dimensions
