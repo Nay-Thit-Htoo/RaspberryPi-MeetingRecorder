aac94d6783f53dc10bd501ca5a7e08294c0bb2e2
Nay Thit Htoo <naythithtoo@abank.com.mm>
2025-02-26 22:34:35 +0630

    update file upload service

--- meeting_vote_service.py
+++ meeting_vote_service.py
@@ -75,7 +75,7 @@ def check_create_newvote_result():
        reset_meeting_vote_result()
     last_record=vote_result['meeting_vote_result'][-1]
     if(not last_record is None):
-        last_record_date=datetime.strptime(last_record['created_date'], "%d-%m-%Y")
+        last_record_date=datetime.strptime(last_record['created_date'], "%d-%m-%Y %H:%M:%S").date()
         current_date=(datetime.now()).strftime("%d-%m-%Y")
         if(last_record_date!=current_date):
             reset_meeting_vote_result()
