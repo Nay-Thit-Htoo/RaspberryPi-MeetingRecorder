b59a3eb16c32f25055f428f349b4a926fb91ea75
Nay Thit Htoo <naythithtoo@abank.com.mm>
2025-03-01 11:32:19 +0630

    update chairman stop btn name

--- meeting_record_chairman.py
+++ meeting_record_chairman.py
@@ -332,6 +332,7 @@ class MeetingRecordChairman(tk.Frame):
     def stop_recording(self):          
        if(self.startBtn.cget("text").lower() =='discussing'):            
             self.meeting_status_label.config(text="")
+            self.startBtn.config(text="Discuss")
             self.logged_user_info=clientservice.read_clientInfo()
             meeting_record_obj={"usercode":self.logged_user_info['usercode'],
                     "usertype":self.logged_user_info['usertype'],
